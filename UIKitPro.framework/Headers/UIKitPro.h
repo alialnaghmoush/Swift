f73da62157947b4a92c6a9f7d7860a85e2f654a8
Ali AlNaghmousgh <alialnaghmoush@gmail.com>
2018-02-14 08:51:09 +0300

    Add files via upload

---
+++ UIKitPro.framework/Headers/UIKitPro.h
@@ -0,0 +1,19 @@
+//
+//  UIKitPro.h
+//  UIKitPro
+//
+//  Created by Ali AlNaghmoush on 14/02/2018.
+//  Copyright © 2018 Ali AlNaghmoush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKitPro.
+FOUNDATION_EXPORT double UIKitProVersionNumber;
+
+//! Project version string for UIKitPro.
+FOUNDATION_EXPORT const unsigned char UIKitProVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitPro/PublicHeader.h>
+
+
